a8303d40383a861acb50211dea1b3dc877bdcea2
AlexandraShulga <a.shulga@pbfgroup.ru>
2025-09-14 16:01:20 +0300

    278. First Bad Version

--- src/BinarySearch.kt
+++ src/BinarySearch.kt
@@ -44,7 +44,7 @@ Input: nums = [1,3,5,6], target = 7
 Output: 4
  **/
 
-class Solution {
+class Solution1 {
     fun searchInsert(nums: IntArray, target: Int): Int {
         var startIndex = 0
         var finishIndex = nums.size - 1
@@ -54,6 +54,64 @@ class Solution {
                 nums[midIndex] == target -> return midIndex
                 nums[midIndex] < target -> startIndex = midIndex + 1
                 else -> finishIndex = midIndex - 1
+            }
+        }
+        return startIndex
+    }
+}
+
+
+/**
+ * 278. First Bad Version
+ * Easy
+ * Topics
+ * premium lock icon
+ * Companies
+ * You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+ *
+ * Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+ *
+ * You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: n = 5, bad = 4
+ * Output: 4
+ * Explanation:
+ * call isBadVersion(3) -> false
+ * call isBadVersion(5) -> true
+ * call isBadVersion(4) -> true
+ * Then 4 is the first bad version.
+ * Example 2:
+ *
+ * Input: n = 1, bad = 1
+ * Output: 1
+ */
+
+
+/* The isBadVersion API is defined in the parent class VersionControl.
+      fun isBadVersion(version: Int) : Boolean {} */
+
+abstract class VersionControl() {
+    open fun isBadVersion(version: Int): Boolean = version >= 4
+    abstract fun firstBadVersion(n: Int): Int
+
+}
+
+class Solution : VersionControl() {
+    override fun firstBadVersion(n: Int): Int {
+        var startIndex = 1
+        var finishIndex = n
+
+        while (startIndex <= finishIndex) {
+            val midIndex = startIndex + (finishIndex - startIndex) / 2
+
+
+            when (isBadVersion(midIndex)) {
+                false -> startIndex = midIndex + 1
+                true -> finishIndex = midIndex - 1
             }
         }
         return startIndex
