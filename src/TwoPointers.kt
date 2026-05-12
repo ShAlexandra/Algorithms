c152b121c18ed14c1affd63a0ff2e73869b61ea5
a.shulga <a.shulga@pbfgroup.ru>
2026-05-12 23:14:47 +0300

    125. Valid Palindrome

--- src/TwoPointers.kt
+++ src/TwoPointers.kt
@@ -1,11 +1,6 @@
 /**
  * 344. Reverse String
- * Solved
- * Easy
- * Topics
- * premium lock icon
- * Companies
- * Hint
+ *
  * Write a function that reverses a string. The input string is given as an array of characters s.
  *
  * You must do this by modifying the input array in-place with O(1) extra memory.
@@ -26,15 +21,64 @@
  *
  * 1 <= s.length <= 105
  */
+
 fun reverseString(s: CharArray) {
     var p1 = 0
     var p2 = s.size - 1
 
-    while (p1<p2) {
+    while (p1 < p2) {
         val temp = s[p1]
         s[p1] = s[p2]
         s[p2] = temp
         p1++
         p2--
     }
+}
+
+/**
+ *
+ * 125. Valid Palindrome
+ *
+ * A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+ *
+ * Given a string s, return true if it is a palindrome, or false otherwise.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: s = "A man, a plan, a canal: Panama"
+ * Output: true
+ * Explanation: "amanaplanacanalpanama" is a palindrome.
+ * Example 2:
+ *
+ * Input: s = "race a car"
+ * Output: false
+ * Explanation: "raceacar" is not a palindrome.
+ * Example 3:
+ *
+ * Input: s = " "
+ * Output: true
+ * Explanation: s is an empty string "" after removing non-alphanumeric characters.
+ * Since an empty string reads the same forward and backward, it is a palindrome.
+ */
+
+fun isPalindrome(s: String): Boolean {
+    var p1 = 0
+    var p2 = s.length - 1
+
+    while (p1 < p2) {
+        when {
+            s[p1].isLetterOrDigit() && s[p2].isLetterOrDigit() -> {
+                if (s[p1].lowercaseChar() != s[p2].lowercaseChar()) return false
+
+                    p1++
+                    p2--
+            }
+
+            !s[p1].isLetterOrDigit() -> p1++
+            !s[p2].isLetterOrDigit() -> p2--
+        }
+    }
+    return true
 }
