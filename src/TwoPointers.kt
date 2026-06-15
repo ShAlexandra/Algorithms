e12ab787eb81a973c47e9317a9c7c2bb521d2136
a.shulga <a.shulga@pbfgroup.ru>
2026-06-15 15:23:50 +0300

    283. Move Zeroes

--- src/TwoPointers.kt
+++ src/TwoPointers.kt
@@ -247,3 +247,60 @@ fun removeDuplicates(nums: IntArray): Int {
     }
     return p + 1
 }
+
+/**
+ * 283. Move Zeroes
+ * Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+ *
+ * Note that you must do this in-place without making a copy of the array.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [0,1,0,3,12]
+ * Output: [1,3,12,0,0]
+ * Example 2:
+ *
+ * Input: nums = [0]
+ * Output: [0]
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= nums.length <= 104
+ * -231 <= nums[i] <= 231 - 1
+ */
+
+fun moveZeroesRwoPointers(nums: IntArray): Unit {
+    var p1 = 0
+    var p2 = 1
+
+    while (p2 < nums.size) {
+        if (nums[p1] == 0) {
+            if (nums[p2] == 0) {
+                p2++
+            } else {
+                nums[p1] = nums[p2]
+                nums[p2] = 0
+                p1++
+                p2++
+            }
+        } else {
+            p1++
+            p2++
+        }
+    }
+}
+
+fun moveZeroes(nums: IntArray): Unit {
+    var insertPos = 0
+    for (num in nums) {
+        if (num != 0) {
+            nums[insertPos++] = num
+        }
+    }
+    while (insertPos < nums.size) {
+        nums[insertPos++] = 0
+    }
+}
