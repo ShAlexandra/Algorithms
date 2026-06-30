468c0ce9d943f6b8a243665340c06c12534d564f
AlexandraShulga <a.shulga@pbfgroup.ru>
2026-06-30 14:28:48 +0300

    443. String Compression

--- src/TwoPointers.kt
+++ src/TwoPointers.kt
@@ -416,3 +416,99 @@ fun threeSum(nums: IntArray): List<List<Int>> {
     }
     return listResult
 }
+
+
+/**
+ *
+ * 443. String Compression
+ * Given an array of characters chars, compress it using the following algorithm:
+ *
+ * Begin with an empty string s. For each group of consecutive repeating characters in chars:
+ *
+ * If the group's length is 1, append the character to s.
+ * Otherwise, append the character followed by the group's length.
+ * The compressed string s should not be returned separately, but instead, be stored in the input character array chars. Note that group lengths that are 10 or longer will be split into multiple characters in chars.
+ *
+ * After you are done modifying the input array, return the new length of the array.
+ *
+ * You must write an algorithm that uses only constant extra space.
+ *
+ * Note: The characters in the array beyond the returned length do not matter and should be ignored.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: chars = ["a","a","b","b","c","c","c"]
+ * Output: 6
+ * Explanation: The groups are "aa", "bb", and "ccc". This compresses to "a2b2c3".
+ * After modifying the input array in-place, the first 6 characters of chars should be ["a","2","b","2","c","3"].
+ * Example 2:
+ *
+ * Input: chars = ["a"]
+ * Output: 1
+ * Explanation: The only group is "a", which remains uncompressed since it is a single character.
+ * After modifying the input array in-place, the first character of chars should be ["a"].
+ * Example 3:
+ *
+ * Input: chars = ["a","b","b","b","b","b","b","b","b","b","b","b","b"]
+ * Output: 4
+ * Explanation: The groups are "a" and "bbbbbbbbbbbb". This compresses to "ab12".
+ * After modifying the input array in-place, the first 4 characters of chars should be ["a","b","1","2"].
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= chars.length <= 2000
+ * chars[i] is a lowercase English letter, uppercase English letter, digit, or symbol.
+ */
+fun compressTwoPointers(chars: CharArray): Int {
+    var p1 = 0
+    var p2 = 0
+    var p = 0
+    while (p2 < chars.size) {
+        if (chars[p1]==chars[p2]) {
+            p2++
+        } else {
+            chars[p++]=chars[p1]
+            if (p2-p1>1) {
+                val count = (p2-p1).toString()
+                for (i in count) {
+                    chars[p++] = i
+                }
+            }
+            p1 = p2
+        }
+    }
+    if (p2>p1) {
+        chars[p++] = chars[p1]
+        if (p2-p1>1) {
+            val count = (p2-p1).toString()
+            for (i in count) {
+                chars[p++] = i
+            }
+        }
+    }
+    return p
+}
+
+fun compress(chars: CharArray): Int {
+    var read = 0
+    var write = 0
+
+    while (read<chars.size) {
+        val current = chars[read++]
+        var count = 1
+        while (read<chars.size && chars[read] == current) {
+            read++
+            count++
+        }
+        chars[write++] = current
+        if (count>1) {
+            for (i in count.toString()) {
+                chars[write++] = i
+            }
+        }
+    }
+    return write
+}
