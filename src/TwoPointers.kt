788c482adc079462e3bc516c1d1881875e9af0ae
AlexandraShulga <a.shulga@pbfgroup.ru>
2026-07-02 13:00:40 +0300

    11. Container With Most Water

--- src/TwoPointers.kt
+++ src/TwoPointers.kt
@@ -583,3 +583,48 @@ fun compareVersion(version1: String, version2: String): Int {
     }
     return 0
 }
+
+
+/**
+ *
+ * 11. Container With Most Water
+ * You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+ *
+ * Find two lines that together with the x-axis form a container, such that the container contains the most water.
+ *
+ * Return the maximum amount of water a container can store.
+ *
+ * Notice that you may not slant the container.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: height = [1,8,6,2,5,4,8,3,7]
+ * Output: 49
+ * Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+ * Example 2:
+ *
+ * Input: height = [1,1]
+ * Output: 1
+ */
+fun maxArea(height: IntArray): Int {
+    var max = 0
+    var p1 = 0
+    var p2 = height.size-1
+    while (p1<p2) {
+        if (height[p1]>height[p2]) {
+            if (height[p2]*(p2-p1)>max) {
+                max = height[p2] * (p2 - p1)
+            }
+            p2--
+        } else {
+            if (height[p1]*(p2-p1)>max) {
+                max = height[p1] * (p2 - p1)
+            }
+            p1++
+        }
+    }
+    return max
+}
