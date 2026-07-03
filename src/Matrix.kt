8249ef2b53b603e9496a19c867b904997dc3bd9d
AlexandraShulga <a.shulga@pbfgroup.ru>
2026-07-03 13:26:19 +0300

    867. Transpose Matrix

--- src/Matrix.kt
+++ src/Matrix.kt
@@ -45,3 +45,47 @@ fun diagonalSum(mat: Array<IntArray>): Int {
     }
     return sum
 }
+
+/**
+ * 867. Transpose Matrix
+ * Easy
+ * Topics
+ * premium lock icon
+ * Companies
+ * Hint
+ * Given a 2D integer array matrix, return the transpose of matrix.
+ *
+ * The transpose of a matrix is the matrix flipped over its main diagonal, switching the matrix's row and column indices.
+ *
+ *
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+ * Output: [[1,4,7],[2,5,8],[3,6,9]]
+ * Example 2:
+ *
+ * Input: matrix = [[1,2,3],[4,5,6]]
+ * Output: [[1,4],[2,5],[3,6]]
+ *
+ *
+ * Constraints:
+ *
+ * m == matrix.length
+ * n == matrix[i].length
+ * 1 <= m, n <= 1000
+ * 1 <= m * n <= 105
+ * -109 <= matrix[i][j] <= 109
+ */
+
+fun transpose(matrix: Array<IntArray>): Array<IntArray> {
+    val resultMatrix = Array(matrix[0].size){ IntArray(matrix.size) }
+    for (i in 0..<matrix[0].size) {
+        for (j in 0..<matrix.size) {
+            resultMatrix[i][j] = matrix[j][i]
+        }
+    }
+    return resultMatrix
+}
