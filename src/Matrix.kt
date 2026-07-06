dbe05b8b84786b6ece76214cd679c65548841777
a.shulga <a.shulga@pbfgroup.ru>
2026-07-06 17:07:19 +0300

    36. Valid Sudoku 2 variants

--- src/Matrix.kt
+++ src/Matrix.kt
@@ -38,10 +38,10 @@
 fun diagonalSum(mat: Array<IntArray>): Int {
     var sum = 0
     for (i in 0..<mat.size) {
-        sum+=(mat[i][i]+mat[i][mat.size-1-i])
+        sum += (mat[i][i] + mat[i][mat.size - 1 - i])
     }
-    if (mat.size%2==1) {
-        sum-=mat[(mat.size-1)/2][(mat.size-1)/2]
+    if (mat.size % 2 == 1) {
+        sum -= mat[(mat.size - 1) / 2][(mat.size - 1) / 2]
     }
     return sum
 }
@@ -81,11 +81,86 @@ fun diagonalSum(mat: Array<IntArray>): Int {
  */
 
 fun transpose(matrix: Array<IntArray>): Array<IntArray> {
-    val resultMatrix = Array(matrix[0].size){ IntArray(matrix.size) }
+    val resultMatrix = Array(matrix[0].size) { IntArray(matrix.size) }
     for (i in 0..<matrix[0].size) {
         for (j in 0..<matrix.size) {
             resultMatrix[i][j] = matrix[j][i]
         }
     }
     return resultMatrix
+}
+
+/**
+ *
+ * 36. Valid Sudoku
+ * Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+ *
+ * Each row must contain the digits 1-9 without repetition.
+ * Each column must contain the digits 1-9 without repetition.
+ * Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+ * Note:
+ *
+ * A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+ * Only the filled cells need to be validated according to the mentioned rules.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: board =
+ * [["5","3",".",".","7",".",".",".","."]
+ * ,["6",".",".","1","9","5",".",".","."]
+ * ,[".","9","8",".",".",".",".","6","."]
+ * ,["8",".",".",".","6",".",".",".","3"]
+ * ,["4",".",".","8",".","3",".",".","1"]
+ * ,["7",".",".",".","2",".",".",".","6"]
+ * ,[".","6",".",".",".",".","2","8","."]
+ * ,[".",".",".","4","1","9",".",".","5"]
+ * ,[".",".",".",".","8",".",".","7","9"]]
+ * Output: true
+ * Example 2:
+ *
+ * Input: board =
+ * [["8","3",".",".","7",".",".",".","."]
+ * ,["6",".",".","1","9","5",".",".","."]
+ * ,[".","9","8",".",".",".",".","6","."]
+ * ,["8",".",".",".","6",".",".",".","3"]
+ * ,["4",".",".","8",".","3",".",".","1"]
+ * ,["7",".",".",".","2",".",".",".","6"]
+ * ,[".","6",".",".",".",".","2","8","."]
+ * ,[".",".",".","4","1","9",".",".","5"]
+ * ,[".",".",".",".","8",".",".","7","9"]]
+ * Output: false
+ * Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+ */
+
+fun isValidSudoku(board: Array<CharArray>): Boolean {
+    for (i in 0..<board.size) {
+        for (j in 0..<board[0].size) {
+            val current = board[i][j]
+            if (current != '.') {
+                for (k in j + 1..<board[0].size) {
+                    if (current == board[i][k]) {
+                        return false
+                    }
+                }
+                for (k in i + 1..<board.size) {
+                    if (board[k][j] == current) {
+                        return false
+                    }
+                }
+
+                val startRow: Int = i / 3 * 3
+                val startColumn: Int = j / 3 * 3
+                for (k in startRow..startRow + 2) {
+                    for (h in startColumn..startColumn + 2) {
+                        if ((i != k || j != h) && current == board[k][h]) {
+                            return false
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return true
 }
