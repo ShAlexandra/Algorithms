26bcab068537d8060b7c323f3d913cc565480597
a.shulga <a.shulga@pbfgroup.ru>
2026-07-14 22:58:44 +0300

    205. Isomorphic Strings

--- src/HashTable.kt
+++ src/HashTable.kt
@@ -132,3 +132,56 @@ fun twoSum(nums: IntArray, target: Int): IntArray {
     }
     return intArrayOf()
 }
+
+/**
+ * 205. Isomorphic Strings
+ * Given two strings s and t, determine if they are isomorphic.
+ *
+ * Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+ *
+ * All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: s = "egg", t = "add"
+ *
+ * Output: true
+ *
+ * Explanation:
+ *
+ * The strings s and t can be made identical by:
+ *
+ * Mapping 'e' to 'a'.
+ * Mapping 'g' to 'd'.
+ * Example 2:
+ *
+ * Input: s = "f11", t = "b23"
+ *
+ * Output: false
+ *
+ * Explanation:
+ *
+ * The strings s and t can not be made identical as '1' needs to be mapped to both '2' and '3'.
+ *
+ * Example 3:
+ *
+ * Input: s = "paper", t = "title"
+ *
+ * Output: true
+ */
+
+fun isIsomorphic(s: String, t: String): Boolean {
+    val mapS = mutableMapOf<Char, Int>()
+    val mapT = mutableMapOf<Char, Int>()
+
+    for (i in s.indices) {
+        if (mapS[s[i]]!=mapT[t[i]]) {
+            return false
+        }
+        mapS[s[i]]=i
+        mapT[t[i]]=i
+    }
+    return true
+}
