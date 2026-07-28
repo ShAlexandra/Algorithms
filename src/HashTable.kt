02ee2d42206da1adbea677cdb8fdf98a1772cc3a
a.shulga <a.shulga@pbfgroup.ru>
2026-07-28 16:47:14 +0300

    49. Group Anagrams

--- src/HashTable.kt
+++ src/HashTable.kt
@@ -278,6 +278,7 @@ fun value(c: Char) = when (c) {
  * s and t consist of lowercase English letters.
  */
 fun isAnagram(s: String, t: String): Boolean {
+    if (s == "" && t == "") return false
     if (s.length != t.length) return false
     val checkMap = mutableMapOf<Char, Int>()
     s.forEach {
@@ -298,4 +299,54 @@ fun isAnagram(s: String, t: String): Boolean {
     }
 
     return true
+}
+
+
+/**
+ * 49. Group Anagrams
+ * Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: strs = ["eat","tea","tan","ate","nat","bat"]
+ *
+ * Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+ *
+ * Explanation:
+ *
+ * There is no string in strs that can be rearranged to form "bat".
+ * The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
+ * The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.
+ * Example 2:
+ *
+ * Input: strs = [""]
+ *
+ * Output: [[""]]
+ *
+ * Example 3:
+ *
+ * Input: strs = ["a"]
+ *
+ * Output: [["a"]]
+ *
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= strs.length <= 104
+ * 0 <= strs[i].length <= 100
+ * strs[i] consists of lowercase English letters.
+ */
+fun groupAnagrams(strs: Array<String>): List<List<String>> {
+    val mapStrs = mutableMapOf<String, MutableList<String>>()
+    for (str in strs) {
+        val key = str.toCharArray()
+            .sorted()
+            .joinToString("")
+        mapStrs.getOrPut(key) { mutableListOf() }.add(str)
+    }
+
+    return mapStrs.values.toList()
 }
