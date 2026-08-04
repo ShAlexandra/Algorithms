0b2106aa5da36da7298fbba31c00daa69bc1ff44
a.shulga <a.shulga@pbfgroup.ru>
2026-08-04 18:04:38 +0300

    1732. Find the Highest Altitude

--- src/PrefixSum.kt
+++ src/PrefixSum.kt
@@ -24,16 +24,47 @@
  * numArray.sumRange(0, 5); // return (-2) + 0 + 3 + (-5) + 2 + (-1) = -3
  */
 class NumArray(nums: IntArray) {
-    private val prefix = IntArray(nums.size+1)
+    private val prefix = IntArray(nums.size + 1)
 
     init {
         for ((i, element) in nums.withIndex()) {
-            prefix[i+1] = prefix[i] + element
+            prefix[i + 1] = prefix[i] + element
         }
     }
 
     fun sumRange(left: Int, right: Int): Int {
-        return prefix[right+1]-prefix[left]
+        return prefix[right + 1] - prefix[left]
     }
 
+}
+
+/**
+ * 1732. Find the Highest Altitude
+ * There is a biker going on a road trip. The road trip consists of n + 1 points at various altitudes. The biker starts his trip on point 0 with altitude equal 0.
+ *
+ * You are given an integer array gain of length n where gain[i] is the net gain in altitude between points i​​​​​​ and i + 1 for all (0 <= i < n). Return the highest altitude of a point.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: gain = [-5,1,5,0,-7]
+ * Output: 1
+ * Explanation: The altitudes are [0,-5,-4,1,1,-6]. The highest is 1.
+ * Example 2:
+ *
+ * Input: gain = [-4,-3,-2,-1,4,3,2]
+ * Output: 0
+ * Explanation: The altitudes are [0,-4,-7,-9,-10,-6,-3,-1]. The highest is 0.
+ */
+fun largestAltitude(gain: IntArray): Int {
+    var lastHeight = 0
+    var max = lastHeight
+    for (element in gain) {
+        lastHeight += element
+        if (lastHeight>max) {
+            max = lastHeight
+        }
+    }
+    return max
 }
