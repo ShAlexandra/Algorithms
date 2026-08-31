a01f46f3ede172239fbe0d8c5f7fa0799d26e360
AlexandraShulga <a.shulga@pbfgroup.ru>
2026-08-31 12:35:17 +0300

    560. Subarray Sum Equals K

--- src/PrefixSum.kt
+++ src/PrefixSum.kt
@@ -145,3 +145,35 @@ class NumMatrix(matrix: Array<IntArray>) {
     }
 
 }
+
+/**
+ * 560. Subarray Sum Equals K
+ * Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+ *
+ * A subarray is a contiguous non-empty sequence of elements within an array.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [1,1,1], k = 2
+ * Output: 2
+ * Example 2:
+ *
+ * Input: nums = [1,2,3], k = 3
+ * Output: 2
+ */
+
+fun subarraySum(nums: IntArray, k: Int): Int {
+    val prMap = mutableMapOf<Int, Int>()
+    prMap[0]=1
+    var prefix = 0
+    var count = 0
+
+    for (i in 0..<nums.size) {
+        prefix+=nums[i]
+        count+=prMap[prefix-k]?:0
+        prMap[prefix] = prMap[prefix]?.plus(1) ?: 1
+    }
+    return count
+}
