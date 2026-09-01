fb5bd796c106e35301b190535b025aac054e31a6
AlexandraShulga <a.shulga@pbfgroup.ru>
2026-09-01 13:26:25 +0300

    136. Single Number

--- src/BitManipulation.kt
+++ src/BitManipulation.kt
@@ -25,3 +25,42 @@
 fun isPowerOfTwo(n: Int): Boolean {
     return (n > 0 && (n and (n - 1) == 0))
 }
+
+/**
+ * 136. Single Number
+ * Easy
+ * Topics
+ * premium lock icon
+ * Companies
+ * Hint
+ * Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+ *
+ * You must implement a solution with a linear runtime complexity and use only constant extra space.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [2,2,1]
+ *
+ * Output: 1
+ *
+ * Example 2:
+ *
+ * Input: nums = [4,1,2,1,2]
+ *
+ * Output: 4
+ *
+ * Example 3:
+ *
+ * Input: nums = [1]
+ *
+ * Output: 1
+ */
+fun singleNumber(nums: IntArray): Int {
+    var result = 0
+    for (i in nums) {
+        result = result xor i
+    }
+    return result
+}
