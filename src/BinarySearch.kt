04b22bfb8dc267de876d9666726f95cf690d5319
AlexandraShulga <a.shulga@pbfgroup.ru>
2026-09-04 13:07:38 +0300

    69. Sqrt(x)

--- src/BinarySearch.kt
+++ src/BinarySearch.kt
@@ -188,3 +188,41 @@ fun search(nums: IntArray, target: Int): Int {
     }
     return -1
 }
+
+/**
+ * 69. Sqrt(x)
+ * Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+ *
+ * You must not use any built-in exponent function or operator.
+ *
+ * For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+ *
+ *
+ * Example 1:
+ *
+ * Input: x = 4
+ * Output: 2
+ * Explanation: The square root of 4 is 2, so we return 2.
+ * Example 2:
+ *
+ * Input: x = 8
+ * Output: 2
+ * Explanation: The square root of 8 is 2.82842..., and since we round it down to the nearest integer, 2 is returned.
+ */
+fun mySqrt(x: Int): Int {
+    if (x < 2) return x
+    var start = 0
+    var finish = x / 2
+    var mid = finish / 2
+
+    while (start <= finish) {
+        val square = mid.toLong() * mid
+        if (square > x) {
+            finish = mid - 1
+        } else if (square < x) {
+            start = mid + 1
+        } else return mid
+        mid = start + (finish - start) / 2
+    }
+    return finish
+}
