69d66dcc4041eb14fd47845431e65dc974af3163
AlexandraShulga <a.shulga@pbfgroup.ru>
2026-09-05 13:31:22 +0300

    34. Find First and Last Position of Element in Sorted Array

--- src/BinarySearch.kt
+++ src/BinarySearch.kt
@@ -269,3 +269,64 @@ fun isPerfectSquare(num: Int): Boolean {
     }
     return false
 }
+
+/**
+ * 34. Find First and Last Position of Element in Sorted Array
+ * Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+ *
+ * If target is not found in the array, return [-1, -1].
+ *
+ * You must write an algorithm with O(log n) runtime complexity.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [5,7,7,8,8,10], target = 8
+ * Output: [3,4]
+ * Example 2:
+ *
+ * Input: nums = [5,7,7,8,8,10], target = 6
+ * Output: [-1,-1]
+ * Example 3:
+ *
+ * Input: nums = [], target = 0
+ * Output: [-1,-1]
+ */
+fun searchRange(nums: IntArray, target: Int): IntArray {
+    var start = 0
+    var finish = nums.size - 1
+    var mid = finish / 2
+    while (start <= finish) {
+        if (nums[mid] < target) {
+            start = mid + 1
+        } else if (nums[mid] > target) {
+            finish = mid - 1
+        } else {
+            var leftStart = start
+            var leftFinish = mid
+            while (leftStart <= leftFinish) {
+                val leftMid = leftStart + (leftFinish - leftStart) / 2
+                if (nums[leftMid] == target) {
+                    leftFinish = leftMid-1
+                } else {
+                    leftStart = leftMid + 1
+                }
+            }
+
+            var rightStart = mid
+            var rightFinish=finish
+            while (rightStart<=rightFinish) {
+                val rightMid = rightStart+(rightFinish-rightStart)/2
+                if (nums[rightMid]==target){
+                    rightStart = rightMid+1
+                } else {
+                    rightFinish = rightMid-1
+                }
+            }
+            return intArrayOf(leftStart, rightFinish)
+        }
+        mid = start + (finish - start) / 2
+    }
+    return intArrayOf(-1, -1)
+}
